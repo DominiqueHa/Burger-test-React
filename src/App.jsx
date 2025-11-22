049efc03ff59e3fdc4ed4921f105793b31941a37
DominiqueHa <arnaudrome@mail.com>
2025-11-22 19:01:07 +0100

    🎉 F01 : Debut de travail

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import Login from "../components/Login";
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  // States(état, données)
+  const [prenom, setPrenom] = useState("Vi");
 
+  // comportement
+  const handleClick = () => {
+    alert("Vous avez cliqué !");
+    setCount(count + 1);
+  };
+
+  // affichage (render)
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Salut Roméo</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div>
+      <h1>Bonjour {prenom} !</h1>
+      <p>{count}</p>
+      <button onClick={handleClick}>Incrémenter</button>
+      <Login />
+    </div>
+  );
 }
 
-export default App
+export default App;
